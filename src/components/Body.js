65db45e6c1de86f100ce63aab900d71ed2a34e0e
Arpit Seth <setharpit11@gmail.com>
2024-02-11 13:05:49 +0530

    Used fetch to get data from external API with resolving CORS issue using extension

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
-import resData from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurant, setListOfRestaurant] = useState(resData);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  //  ONE WAY WE CAN ADD A LOADER TILL DATA GET POPULATED
+  // if (listOfRestaurant.length === 0) {
+  //   return <h1>Loading.....</h1>;
+  // }
+
+  //SECOND AND RECOMMENDED WAY WILL BE TO SHOW SHIMMER UI TO GIVE PYSCHOLOGICAL EFFECT LIKE WHAT IS COMING
+  if (listOfRestaurant.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
       <div className="filter">
